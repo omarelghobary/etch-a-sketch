d9e664169b4819750035d8fc1800ecc237d37863
Omar <omelghobary@gmail.com>
2022-02-20 17:54:52 +0200

    Added Reset Button

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 const container = document.querySelector('#container');
 
-for (i =0; i<=255; i++ ){
+for (i =0; i< 256; i++ ){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
 }
+
+const myDiv = document.querySelectorAll('.square');
+
+for (let i = 0 ; i < 256; i++){
+    myDiv[i].addEventListener('mouseenter', () => {   
+        myDiv[i].style.backgroundColor = 'black';    
+    });
+}
+
+const reset = document.querySelector('.reset');
+
+    reset.addEventListener('click', () =>{
+        for ( let i =0; i<256; i++){
+            myDiv[i].style.backgroundColor = '#7df3f9';    
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
